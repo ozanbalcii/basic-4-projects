6c545449cbdf680cae2086c16fec4ac424771955
ozan <ozanbalcieem@gmail.com>
2022-11-25 15:13:18 +0300

    Create film.js

---
+++ 2todoListProject/es6-part2/film copy ES6/film.js
@@ -0,0 +1,18 @@
+ // film construction'ı olusturduk:
+// function Film(title, director, url){
+//     this.title = title;
+//     this.director = director;
+//     this.url = url;
+
+// }
+
+// constructor olusturduk
+class Film {
+    constructor(title, director, url){
+        this.title = title;
+        this.director = director;
+        this.url = url;
+    }
+}
+
+
