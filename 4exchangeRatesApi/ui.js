c40367db71f0fd815eb3f416f1f5440cb82932a9
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2022-11-25 15:51:58 +0300

    Update ui.js

--- 4exchangeRatesApi/ui.js
+++ 4exchangeRatesApi/ui.js
@@ -1,26 +1,17 @@
-// websitesinde yazdırılacak olan her şey UI kısmında olcak:
-
 class UI{
     constructor(firstSelect, secondSelect){
         this.firstSelect = firstSelect;
         this.secondSelect = secondSelect;
-
         this.outputFirst=document.createElement("outputFirst");
         this.outputFirst=document.createElement("outputSecond");
         this.outputFirst=document.createElement("outputResult");
-
         }
 
-        // seçtiğimiz kur çeşidine göre EU-TR veya USD-EU yazıo
         changeFirst(){
-
-            // seçilen indekse ve options'a göre textcontent'i seçebiliyoruz
             this.outputFirst.textContent=this.firstSelect.options[this.firstSelect.selectedIndex].textContent
         }
         changeSecond(){
-               // seçilen indekse ve options'a göre textcontent'i seçebiliyoruz
             this.outputSecond.textContent=this.secondSelect.options[this.secondSelect.selectedIndex].textContent
-
         }
         displayResult(result){
             this.outputResult.value=result;
